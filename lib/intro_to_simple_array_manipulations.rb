25af3a6a69be4b5546b69f954ee4d95bd53c142a
Avitosh Totaram <a.totaram@gmail.com>
2019-05-05 23:42:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,3 +12,14 @@ end
 def pop_with_args(array)
     array.pop(2)
 end
+def using_shift(array)
+return array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array1,array2)
+    array1.concat(array2)
+  end
